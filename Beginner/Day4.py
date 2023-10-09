278a28d846f8f5f9cdfa8296195c8589586f1187
Elliot Galibert <elliotgalibert@gmail.com>
2023-10-09 18:03:47 +0300

    started day4

---
+++ Beginner/Day4.py
@@ -0,0 +1,15 @@
+# EX1
+
+# You are going to write a virtual coin toss program. 
+# It will randomly tell the user "Heads" or "Tails".
+
+import random
+
+number = random.randint(0, 1)
+if number == 0:
+	print(f"Heads")
+else:
+	print(f"Tails")
+
+# EX2
+
