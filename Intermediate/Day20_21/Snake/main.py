a80470283098f0c24683cbc10d4a8100734fedf3
Elliot Galibert <elliotgalibert@gmail.com>
2023-10-31 10:13:55 +0200

    Snake game finished

--- Intermediate/Day20_21/Snake/main.py
+++ Intermediate/Day20_21/Snake/main.py
@@ -1,6 +1,8 @@
 from turtle import Screen, Turtle
-import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,7 +11,8 @@ screen.title("Snake")
 
 # screen.update()
 snake = Snake()
-
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,8 +23,24 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
 	screen.update()
-	time.sleep(0.001)
-
+	# time.sleep(0.1)
 	snake.move()
+
+	# Detect collision with food
+	if snake.head.distance(food) < 15:
+		food.refresh()
+		snake.extend()
+		scoreboard.increase_score()
+
+	# Detect collisiong with wall
+	if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+		game_is_on = False
+		scoreboard.game_over()
+
+	# Detect collision with tail
+	for segment in snake.segments[1:]:
+		if snake.head.distance(segment) < 10:
+			game_is_on = False
+			scoreboard.game_over()
 
 screen.exitonclick()
