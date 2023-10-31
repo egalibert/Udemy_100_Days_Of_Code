f85c812d93f30865c12e2f73b766bf66e6b82a6f
Elliot Galibert <elliotgalibert@gmail.com>
2023-10-31 17:08:15 +0200

    Day 23 done

--- Intermediate/Day22/Pong/main.py
+++ Intermediate/Day22/Pong/main.py
@@ -1,5 +1,9 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
 WIDTH = 800
 HEIGHT = 600
 
@@ -9,15 +13,40 @@ screen.title("PONG")
 screen.setup(width=WIDTH, height=HEIGHT)
 screen.tracer(0)
 
-paddle = Paddle()
+r_paddle = Paddle(350, 0)
+l_paddle = Paddle(-350, 0)
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(paddle.up, "Up")
-screen.onkey(paddle.down, "Down")
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
 
 game_is_on = True
 while game_is_on:
+	# time.sleep(0.1)
 	screen.update()
+	ball.move_ball()
 
+	# Detect collision
+	if ball.ycor() > 280 or ball.ycor() < -280:
+		ball.bounce_y()
+
+	# Detect collision with paddle
+	if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+		ball.bounce_x()
+
+	# Detect when r_paddle misses
+	if ball.xcor() > 380:
+		scoreboard.l_point()
+		ball.reset_position()
+
+	# Detect when l_paddle misses
+	if ball.xcor() < -380:
+		scoreboard.r_point()
+		ball.reset_position()
 
 screen.exitonclick()
--- Intermediate/Day22/Pong/paddle.py
+++ Intermediate/Day22/Pong/paddle.py
@@ -1,19 +1,19 @@
 from turtle import Turtle
 HEIGHT = 100
 WIDTH = 20
-UP = "Up"
-DOWN = "Down"
+# UP = "Up"
+# DOWN = "Down"
 
 class Paddle(Turtle):
 
-	def __init__(self):
+	def __init__(self, x_position, y_position):
 		super().__init__()
 		self.color("white")
 		self.shape("square")
 		self.shapesize(stretch_wid=5, stretch_len=1)
 		# self.turtlesize(stretch_len=HEIGHT, stretch_wid=WIDTH)
 		self.penup()
-		self.goto(350, 0)
+		self.goto(x_position, y_position)
 		
 	def up(self):
 		new_y = self.ycor() + 20
