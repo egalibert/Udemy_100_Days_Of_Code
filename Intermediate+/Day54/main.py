9049163d45787cc15447c0a8c1f3bcc567966e7b
Elliot Galibert <elliotgalibert@gmail.com>
2023-11-19 17:34:58 +0200

    streak

--- Intermediate+/Day54/main.py
+++ Intermediate+/Day54/main.py
@@ -1,3 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
