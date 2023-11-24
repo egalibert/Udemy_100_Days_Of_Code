4467976bd4115ceecac6f29caaaa7c2ba42adb71
Elliot Galibert <elliotgalibert@gmail.com>
2023-11-24 11:39:25 +0200

    carrying on wiht day 57

--- Intermediate+/Day57/Practice/server.py
+++ Intermediate+/Day57/Practice/server.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 import random
 from datetime import date
+import requests
+
+NAMEURL = "https://api.genderize.io?name=peter"
 
 app = Flask(__name__)
 
@@ -10,5 +13,23 @@ def main():
 	today = date.today().year
 	return render_template("index.html", num=random_number, current_date=today)
 
+@app.route('/guess/<name>')
+def guess(name):
+	gender_url = f"https://api.genderize.io?name={name}"
+	age_url = f"https://api.agify.io?name={name}"
+
+	# Get the gender from genderize api
+	gender_response = requests.get(gender_url)
+	gender_data = gender_response.json()
+	gender = gender_data["gender"]
+
+	# Get the age from agify api
+	age_response = requests.get(age_url)
+	age_data = age_response.json()
+	age = age_data["age"]
+
+	return render_template("guess.html", name=name, gender=gender, age=age)
+
 if __name__ == "__main__":
 	app.run(debug=True)
+
